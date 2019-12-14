27362a29c83d26efbb307d5d05f3ce7b4bde7ccb
alfcastillo90 <cacr1990@gmail.com>
2019-12-14 00:04:12 -0300

    [ADD]: relacion uno a muchos entre paises y ciudades

--- app/Country.php
+++ app/Country.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Country extends Model
 {
     protected $table = 'countries';
-    protected $fillable = ['id','name'];
+    protected $fillable = ['name'];
     protected $dates = ['created_at','updated_at'];
+
+    public function cities()
+    {
+        return $this->hasMany(City::class,'country_id','id');
+    }
+
 }
